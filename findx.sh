6852e2316dc9e43719c483f6e4da0774cf773387
Gyumeijie <18170879819@163.com>
2018-10-21 21:53:34 +0800

    feat: support mulit comma-sparated exec commands
    e.g. -exec 'cmd1, cmd2, cmd3;'

--- findx.sh
+++ findx.sh
@@ -1,7 +1,36 @@
 #! /bin/bash
 
-re='-exec[^;]*;'
+# Sparate comma-sparated exec commands, e.g. -exec 'cmd1, cmd2, cmd3;'
+function sparateExecCommad() {
+  local string=$1
+  local len=${#string}
+  local range=`eval echo {0..$((len-1))}`
+  local quote='CLOSE'
+  local ret=''
 
+  for i in $range;
+  do
+    char=${string:i:1}
+   
+    if [[ "$char" = "\"" ]]; then
+       if [[ $quote = "CLOSE" ]]; then quote="OPEN"; else quote="CLOSE"; fi
+    fi
+
+    if [[ "$char" = "," ]] && [[ $quote = "CLOSE" ]]; then
+        # Replace all unquoted commas with '\n'
+        ret=${ret}'\n'
+        continue
+    fi
+
+    ret=${ret}$char
+  done
+
+  # Prevent expansion and quotes removal
+  echo "$ret"
+}
+
+
+re='-exec[^;]*;'
 if [[ $@ =~ $re ]];
 then
   tempfile="/tmp/tempfile$$.sh"
@@ -9,7 +38,9 @@ then
 
   # Extract -exec comannd and save all them to the tempfile
   echo "#! /bin/bash" >> $tempfile
-  echo -e $(sed "s:[^;]*-exec \([^;]*\);:\1\\\n:g" <<< "$@") >> $tempfile
+  # Multi -exec commands sparated by '\n'
+  exec=$(sed "s:[^;]*-exec \([^;]*\);:\1\\\n:g" <<< "$@")
+  echo -e $(sparateExecCommad "$exec") >> $tempfile
 
   # Reconstruct command: remove -exec commands and append a new one
   cmd=$(sed "s:\(-exec [^;]*;\): :g" <<< "$@")
